58f2596ea978976a512ab2a07e8e391f639e327d
Kelsey Pownell <kepownel@microsoft.com>
2021-06-07 14:55:01 -0700

    add new package to monorepo-packages test

--- packages/common/src/build-utilities/monorepo-packages.spec.ts
+++ packages/common/src/build-utilities/monorepo-packages.spec.ts
@@ -11,6 +11,7 @@ describe('listMonorepoPackageNames', () => {
               "common",
               "logger",
               "resource-deployment",
+              "storage-documents",
             ]
         `);
     });
