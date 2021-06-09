8685cff2305c4d84b448aad7f8f69f6578f79856
Kelsey Pownell <kepownel@microsoft.com>
2021-06-09 11:57:00 -0700

    add other WCP attributes to Website doc

--- packages/storage-documents/src/website.ts
+++ packages/storage-documents/src/website.ts
@@ -5,10 +5,34 @@ import { ScanType } from './scan-types';
 import { StorageDocument } from './storage-document';
 
 export interface Website extends StorageDocument {
+    name: string;
     baseUrl: string;
     priority: number;
     discoveryPatterns: string[];
     knownPages: string[];
     scanners: ScanType[];
-    // other site attributes
+    domainId: string;
+    owners?: string[];
+    orgCvp?: string;
+    usingUhf?: boolean;
+    compliant: boolean;
+    enableCrawler: boolean;
+    deleted: boolean;
+    divisionId?: number;
+    siteStateId?: number;
+    noBanner: boolean;
+    noBannerReasonId?: number;
+    serviceTreeId: string;
+    notes?: string;
+    alias?: string;
+    organizationId?: number;
+    celaAccessibilityContacts?: string[];
+    privagyEGRCExceptionId?: string;
+    AIEGRCExceptionId?: boolean;
+    optOutSecurityReason?: string;
+    isCustomBanner: boolean;
+    isMicrosoftOwned: boolean;
+    requireAuthentication?: boolean;
+    manageCookieXpath?: string;
+    isCookieManagable: boolean;
 }
