fa15017795bf4ff1ccc0776aeac50ca7544c740c
Wahaj Abid <waabid@microsoft.com>
2021-06-14 13:46:10 -0700

    Add export barrel file for storage docs and minor fix

---
+++ packages/storage-documents/src/index.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+export { PageScan } from './page-scan';
+export { Page } from './page';
+export { ReportData, PageReportFormat } from './report-data';
+export { WebsiteScan } from './website-scan';
+export { Website } from './website';
+export { ScanType, ScanStatus } from './scan-types';
+export { StorageDocument } from './storage-document';
