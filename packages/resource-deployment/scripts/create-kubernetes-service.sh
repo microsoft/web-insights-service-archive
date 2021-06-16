d085e2da6b9957d11ab072819760f0824f31941b
Kelsey Pownell <kepownel@microsoft.com>
2021-06-16 12:31:14 -0700

    fix role assignment command

--- packages/resource-deployment/scripts/create-kubernetes-service.sh
+++ packages/resource-deployment/scripts/create-kubernetes-service.sh
@@ -14,13 +14,11 @@ Usage: $0 -r <resource group> -l <location> -c <cluster name> -d <container regi
 
 function attachContainerRegistry() {
     # grant cluster sp access to container registry
-    principalId=$(az aks show --resource-group "$resourceGroupName" --name "$kubernetesServiceName" --query "identity.principalId" -o tsv)
-    role="acrPull"
+    principalId=$(az aks show --resource-group "$resourceGroupName" --name "$kubernetesServiceName" --query "identityProfile.kubeletidentity.objectId" -o tsv)
+    role="AcrPull"
     containerRegistryId=$(az acr show --resource-group "$resourceGroupName" --name "$containerRegistry" --query id -o tsv)
     scope="--scope $containerRegistryId"
     . "${0%/*}/role-assign-for-sp.sh"
-
-    az aks update --resource-group "$resourceGroupName" --name "$kubernetesServiceName" --attach-acr "$containerRegistry" 1>/dev/null
 }
 
 # Read script arguments
@@ -41,6 +39,5 @@ fi
 
 # Deploy Azure Kubernetes Service
 echo "Deploying Azure Kubernetes Service in resource group $resourceGroupName"
-az aks create --resource-group "$resourceGroupName" --name "$kubernetesServiceName" --location "$location" --enable-managed-identity 1>/dev/null
-
+az aks create --resource-group "$resourceGroupName" --name "$kubernetesServiceName" --location "$location" 1>/dev/null
 attachContainerRegistry
