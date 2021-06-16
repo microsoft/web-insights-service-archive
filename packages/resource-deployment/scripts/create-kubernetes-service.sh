fd7a7be03942fa7c8980ea9eb5de29df4c0f3245
pownkel <55459788+pownkel@users.noreply.github.com>
2021-06-16 14:45:33 -0700

    chore: manually enable container registry access (#69)
    * manually grant acr permissions
    
    * fix role assignment command
    
    * use --no-ssh-key

--- packages/resource-deployment/scripts/create-kubernetes-service.sh
+++ packages/resource-deployment/scripts/create-kubernetes-service.sh
@@ -12,6 +12,15 @@ Usage: $0 -r <resource group> -l <location> -c <cluster name> -d <container regi
     exit 1
 }
 
+function attachContainerRegistry() {
+    # grant cluster sp access to container registry
+    principalId=$(az aks show --resource-group "$resourceGroupName" --name "$kubernetesServiceName" --query "identityProfile.kubeletidentity.objectId" -o tsv)
+    role="AcrPull"
+    containerRegistryId=$(az acr show --resource-group "$resourceGroupName" --name "$containerRegistry" --query id -o tsv)
+    scope="--scope $containerRegistryId"
+    . "${0%/*}/role-assign-for-sp.sh"
+}
+
 # Read script arguments
 while getopts ":r:c:l:d:" option; do
     case $option in
@@ -30,4 +39,5 @@ fi
 
 # Deploy Azure Kubernetes Service
 echo "Deploying Azure Kubernetes Service in resource group $resourceGroupName"
-az aks create --resource-group "$resourceGroupName" --name "$kubernetesServiceName" --location "$location" --attach-acr "$containerRegistry" 1>/dev/null
+az aks create --resource-group "$resourceGroupName" --name "$kubernetesServiceName" --location "$location" --no-ssh-key 1>/dev/null
+attachContainerRegistry
