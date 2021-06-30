86609558540bc5432a2b3b111a6af07e267999d6
Maxim Laikine <la.maks@hotmail.com>
2021-06-29 19:41:11 -0700

    Fix AKS access to ACR

--- packages/resource-deployment/scripts/create-kubernetes-service.sh
+++ packages/resource-deployment/scripts/create-kubernetes-service.sh
@@ -13,13 +13,6 @@ Usage: ${BASH_SOURCE} -r <resource group> -l <location> [-c <cluster name>] [-d 
     exit 1
 }
 
-function attachContainerRegistry() {
-    role="AcrPull"
-    containerRegistryId=$(az acr show --resource-group "${resourceGroupName}" --name "${containerRegistry}" --query id -o tsv)
-    scope="--scope ${containerRegistryId}"
-    . "${0%/*}/role-assign-for-sp.sh"
-}
-
 # Read script arguments
 while getopts ":r:c:l:d:" option; do
     case ${option} in
@@ -46,16 +39,13 @@ subscription=$(az account show --query "id" -o tsv)
 # Deploy Azure Kubernetes Service
 echo "Deploying Azure Kubernetes Service in resource group ${resourceGroupName}"
 az aks create --resource-group "${resourceGroupName}" --name "${kubernetesService}" --location "${location}" \
+    --attach-acr "${containerRegistry}" \
     --no-ssh-key --enable-managed-identity --enable-addons monitoring,http_application_routing \
     --workspace-resource-id "/subscriptions/${subscription}/resourcegroups/${resourceGroupName}/providers/microsoft.operationalinsights/workspaces/${monitorWorkspace}" \
     1>/dev/null
 
-principalId=$(az aks show --resource-group "${resourceGroupName}" --name "${kubernetesService}" --query "identity.principalId" -o tsv)
-
-# Grant access to container registry
-attachContainerRegistry
-
 # Grant access to key vault
+principalId=$(az aks show --resource-group "${resourceGroupName}" --name "${kubernetesService}" --query "identity.principalId" -o tsv)
 . "${0%/*}/enable-msi-for-key-vault.sh"
 
 echo "Azure Kubernetes Service successfully created."
