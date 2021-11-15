e2c05cd1769dad75e71bad02e6a0a517f1df29f6
pownkel <55459788+pownkel@users.noreply.github.com>
2021-11-15 13:57:31 -0800

    feat(e2e-tests): Storage API client (#355)
    Create a client to call the storage API, for use in E2E testing

---
+++ packages/storage-api-client/src/web-insights-storage-client.spec.ts
@@ -0,0 +1,211 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import 'reflect-metadata';
+
+import * as ApiContracts from 'api-contracts';
+import { Agents, ExtendOptions, Got, Options } from 'got';
+import { IMock, It, Mock } from 'typemoq';
+import { RetryHelper } from 'common';
+import { Logger } from 'logger';
+import { WebInsightsStorageClient } from './web-insights-storage-client';
+
+describe(WebInsightsStorageClient, () => {
+    const baseUrl = 'base-url';
+    const apiVersion = '1.0';
+    const maxRetryCount = 5;
+    const msecBetweenRetries = 10;
+
+    let gotStub: Got;
+    let extendMock: IMock<(options: Options) => Got>;
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    let getMock: IMock<(url: string, options?: Options) => {}>;
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    let postMock: IMock<(url: string, options?: Options) => {}>;
+    let retryHelperMock: IMock<RetryHelper<unknown>>;
+    let loggerMock: IMock<Logger>;
+    let getAgentsMock: IMock<() => Agents>;
+    let response: unknown;
+    const agentsStub = {};
+
+    let testSubject: WebInsightsStorageClient;
+
+    beforeEach(() => {
+        response = { statusCode: 200 };
+
+        extendMock = Mock.ofInstance(() => gotStub);
+        extendMock.setup((e) => e(It.isAny())).returns(() => gotStub);
+        getMock = Mock.ofInstance(() => {
+            return null;
+        });
+        postMock = Mock.ofInstance(() => {
+            return null;
+        });
+        gotStub = {
+            extend: extendMock.object,
+            get: getMock.object,
+            post: postMock.object,
+        } as Got;
+        loggerMock = Mock.ofType<Logger>();
+        retryHelperMock = Mock.ofType<RetryHelper<unknown>>();
+        getAgentsMock = Mock.ofType<() => Agents>();
+        getAgentsMock.setup((ga) => ga()).returns(() => agentsStub);
+
+        testSubject = new WebInsightsStorageClient(
+            baseUrl,
+            loggerMock.object,
+            false,
+            apiVersion,
+            gotStub,
+            getAgentsMock.object,
+            retryHelperMock.object,
+            maxRetryCount,
+            msecBetweenRetries,
+        );
+    });
+
+    afterEach(() => {
+        retryHelperMock.verifyAll();
+        extendMock.verifyAll();
+        postMock.verifyAll();
+        getMock.verifyAll();
+        getAgentsMock.verifyAll();
+    });
+
+    it.each([true, false])('Constructor sets request parameters when throwOnRequestFailure=%s', (throwOnRequestFailure) => {
+        const defaultRequestOptions: ExtendOptions = {
+            searchParams: {
+                'api-version': apiVersion,
+            },
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            responseType: 'json',
+            throwHttpErrors: throwOnRequestFailure,
+            agent: agentsStub,
+        };
+
+        extendMock.reset();
+        extendMock.setup((e) => e(defaultRequestOptions)).returns(() => gotStub);
+
+        testSubject = new WebInsightsStorageClient(
+            baseUrl,
+            loggerMock.object,
+            throwOnRequestFailure,
+            apiVersion,
+            gotStub,
+            getAgentsMock.object,
+            retryHelperMock.object,
+            maxRetryCount,
+            msecBetweenRetries,
+        );
+    });
+
+    it('postWebsite sends POST request with expected properties', async () => {
+        const website = ApiContracts.websiteWithRequiredProperties;
+        const requestOptions = { json: website };
+        const requestUrl = `${baseUrl}/websites`;
+
+        setupRetryHelperMock();
+        postMock
+            .setup((req) => req(requestUrl, requestOptions))
+            .returns(async () => response)
+            .verifiable();
+
+        const actualResponse = await testSubject.postWebsite(website);
+
+        expect(actualResponse).toBe(response);
+    });
+
+    it('getWebsite sends GET request with expected properties', async () => {
+        const websiteId = 'website id';
+        const requestUrl = `${baseUrl}/websites/${websiteId}`;
+
+        setupRetryHelperMock();
+        getMock
+            .setup((req) => req(requestUrl))
+            .returns(async () => response)
+            .verifiable();
+
+        const actualResponse = await testSubject.getWebsite(websiteId);
+        expect(actualResponse).toBe(response);
+    });
+
+    it('postPage sends POST request with expected properties', async () => {
+        const pageUpdate: ApiContracts.PageUpdate = {
+            pageId: 'page id',
+            disabledScans: ['a11y'],
+        };
+        const requestOptions = { json: pageUpdate };
+        const requestUrl = `${baseUrl}/pages`;
+
+        setupRetryHelperMock();
+        postMock
+            .setup((req) => req(requestUrl, requestOptions))
+            .returns(async () => response)
+            .verifiable();
+
+        const actualResponse = await testSubject.postPage(pageUpdate);
+
+        expect(actualResponse).toBe(response);
+    });
+
+    it('postWebsiteScan sends POST request with expected properties', async () => {
+        const websiteScan: ApiContracts.WebsiteScanRequest = {
+            websiteId: 'website id',
+            scanType: 'a11y',
+        };
+        const requestOptions = { json: websiteScan };
+        const requestUrl = `${baseUrl}/websites/scans`;
+
+        setupRetryHelperMock();
+        postMock
+            .setup((req) => req(requestUrl, requestOptions))
+            .returns(async () => response)
+            .verifiable();
+
+        const actualResponse = await testSubject.postWebsiteScan(websiteScan);
+
+        expect(actualResponse).toBe(response);
+    });
+
+    it('getWebsiteScan sends GET request with specified website id', async () => {
+        const websiteId = 'website id';
+        const scanType = 'a11y';
+        const websiteScanId = 'website scan id';
+        const requestUrl = `${baseUrl}/websites/${websiteId}/scans/${scanType}/${websiteScanId}`;
+
+        setupRetryHelperMock();
+        getMock
+            .setup((req) => req(requestUrl))
+            .returns(async () => response)
+            .verifiable();
+
+        const actualResponse = await testSubject.getWebsiteScan(websiteId, scanType, websiteScanId);
+        expect(actualResponse).toBe(response);
+    });
+
+    it('getLatestWebsiteScan sends GET request with expected url', async () => {
+        const websiteId = 'website id';
+        const scanType = 'a11y';
+        const requestUrl = `${baseUrl}/websites/${websiteId}/scans/${scanType}/latest`;
+
+        setupRetryHelperMock();
+        getMock
+            .setup((req) => req(requestUrl))
+            .returns(async () => response)
+            .verifiable();
+
+        const actualResponse = await testSubject.getLatestWebsiteScan(websiteId, scanType);
+        expect(actualResponse).toBe(response);
+    });
+
+    function setupRetryHelperMock(): void {
+        retryHelperMock
+            .setup((r) => r.executeWithRetries(It.isAny(), It.isAny(), maxRetryCount, msecBetweenRetries))
+            .returns(async (action: () => Promise<unknown>, errorHandler: (error: Error) => Promise<void>, maxAttempts: number) => {
+                return action();
+            })
+            .verifiable();
+    }
+});
---
+++ packages/storage-api-client/src/web-insights-storage-client.ts
@@ -0,0 +1,143 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import * as ApiContracts from 'api-contracts';
+import * as StorageDocuments from 'storage-documents';
+import { getForeverAgents, ResponseWithBodyType, RetryHelper, System } from 'common';
+import { injectable } from 'inversify';
+import { Logger } from 'logger';
+import got, { Agents, ExtendOptions, Got, Options } from 'got';
+
+@injectable()
+export class WebInsightsStorageClient {
+    private readonly baseRequestObj: Got;
+
+    constructor(
+        private readonly baseUrl: string,
+        private readonly logger: Logger,
+        throwOnRequestFailure: boolean = false,
+        apiVersion: string = '1.0',
+        requestObj: Got = got,
+        getAgents: () => Agents = getForeverAgents,
+        private readonly retryHelper: RetryHelper<unknown> = new RetryHelper(),
+        private readonly maxRetryCount: number = 5,
+        private readonly msecBetweenRetries: number = 1000,
+    ) {
+        const defaultRequestOptions: ExtendOptions = {
+            searchParams: {
+                'api-version': apiVersion,
+            },
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            responseType: 'json',
+            throwHttpErrors: throwOnRequestFailure,
+            agent: getAgents(),
+            https: {
+                rejectUnauthorized: false, // Must be false because we use a self-signed cert
+            },
+        };
+
+        this.baseRequestObj = requestObj.extend(defaultRequestOptions);
+    }
+
+    public async postWebsite(website: ApiContracts.Website): Promise<ResponseWithBodyType<ApiContracts.Website>> {
+        const requestUrl = `${this.baseUrl}/websites`;
+        const options: Options = { json: website };
+
+        const executeRequest = async () => this.baseRequestObj.post(requestUrl, options);
+        const onRetry = async (e: Error) =>
+            this.logger.logError('POST website REST API request failed. Retrying on error.', {
+                url: requestUrl,
+                error: System.serializeError(e),
+            });
+
+        return this.executeRequestWithRetries<ApiContracts.Website>(executeRequest, onRetry);
+    }
+
+    public async getWebsite(websiteId: string): Promise<ResponseWithBodyType<ApiContracts.Website>> {
+        const requestUrl = `${this.baseUrl}/websites/${websiteId}`;
+
+        const executeRequest = async () => this.baseRequestObj.get(requestUrl);
+        const onRetry = async (e: Error) =>
+            this.logger.logError('GET website REST API request failed. Retrying on error.', {
+                url: requestUrl,
+                error: System.serializeError(e),
+            });
+
+        return this.executeRequestWithRetries<ApiContracts.Website>(executeRequest, onRetry);
+    }
+
+    public async postPage(pageUpdate: ApiContracts.PageUpdate): Promise<ResponseWithBodyType<ApiContracts.Page>> {
+        const requestUrl = `${this.baseUrl}/pages`;
+        const options: Options = { json: pageUpdate };
+
+        const executeRequest = async () => this.baseRequestObj.post(requestUrl, options);
+        const onRetry = async (e: Error) =>
+            this.logger.logError('POST page REST API request failed. Retrying on error.', {
+                url: requestUrl,
+                error: System.serializeError(e),
+            });
+
+        return this.executeRequestWithRetries<ApiContracts.Page>(executeRequest, onRetry);
+    }
+
+    public async postWebsiteScan(websiteScan: ApiContracts.WebsiteScanRequest): Promise<ResponseWithBodyType<ApiContracts.WebsiteScan>> {
+        const requestUrl = `${this.baseUrl}/websites/scans`;
+        const options: Options = { json: websiteScan };
+
+        const executeRequest = async () => this.baseRequestObj.post(requestUrl, options);
+        const onRetry = async (e: Error) =>
+            this.logger.logError('POST website scan REST API request failed. Retrying on error.', {
+                url: requestUrl,
+                error: System.serializeError(e),
+            });
+
+        return this.executeRequestWithRetries<ApiContracts.WebsiteScan>(executeRequest, onRetry);
+    }
+
+    public async getWebsiteScan(
+        websiteId: string,
+        scanType: StorageDocuments.ScanType,
+        websiteScanId: string,
+    ): Promise<ResponseWithBodyType<ApiContracts.WebsiteScan>> {
+        return this.getWebsiteImpl(websiteId, scanType, websiteScanId);
+    }
+
+    public async getLatestWebsiteScan(
+        websiteId: string,
+        scanType: StorageDocuments.ScanType,
+    ): Promise<ResponseWithBodyType<ApiContracts.WebsiteScan>> {
+        return this.getWebsiteImpl(websiteId, scanType);
+    }
+
+    private async getWebsiteImpl(
+        websiteId: string,
+        scanType: StorageDocuments.ScanType,
+        websiteScanId?: string,
+    ): Promise<ResponseWithBodyType<ApiContracts.WebsiteScan>> {
+        const latestScanTag = 'latest';
+        const requestUrl = `${this.baseUrl}/websites/${websiteId}/scans/${scanType}/${websiteScanId ?? latestScanTag}`;
+
+        const executeRequest = async () => this.baseRequestObj.get(requestUrl);
+        const onRetry = async (e: Error) =>
+            this.logger.logError('GET website scan REST API request failed. Retrying on error.', {
+                url: requestUrl,
+                error: System.serializeError(e),
+            });
+
+        return this.executeRequestWithRetries<ApiContracts.WebsiteScan>(executeRequest, onRetry);
+    }
+
+    private async executeRequestWithRetries<T>(
+        executeRequest: () => Promise<unknown>,
+        onRetry: (e: Error) => Promise<void>,
+    ): Promise<ResponseWithBodyType<T>> {
+        return (await this.retryHelper.executeWithRetries(
+            executeRequest,
+            onRetry,
+            this.maxRetryCount,
+            this.msecBetweenRetries,
+        )) as ResponseWithBodyType<T>;
+    }
+}
