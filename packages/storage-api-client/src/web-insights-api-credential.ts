37fcddd257a8f7927dfcaa87eeea4aeba43dda2a
Kelsey Pownell <kepownel@microsoft.com>
2021-12-07 16:58:04 -0800

    Create WebInsightsApiCredential

---
+++ packages/storage-api-client/src/web-insights-api-credential.ts
@@ -0,0 +1,59 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { AuthenticationResult } from '@azure/msal-common';
+import { ConfidentialClientApplication, Configuration } from '@azure/msal-node';
+import { RetryHelper, System } from 'common';
+import { Headers } from 'got';
+import { Logger } from 'logger';
+
+export type ConfidentialClientApplicationFactory = (config: Configuration) => ConfidentialClientApplication;
+const defaultClientApplicationFactory = (config: Configuration) => new ConfidentialClientApplication(config);
+
+export class WebInsightsAPICredential {
+    private readonly app: ConfidentialClientApplication;
+
+    private readonly tokenScope: string;
+
+    constructor(
+        clientId: string,
+        clientSecret: string,
+        authorityUrl: string,
+        resourceClientId: string,
+        private readonly logger: Logger,
+        private readonly maxTokenAttempts: number = 5,
+        private readonly msecBetweenRetries: number = 1000,
+        private readonly retryHelper: RetryHelper<AuthenticationResult> = new RetryHelper(),
+        clientApplicationFactory: ConfidentialClientApplicationFactory = defaultClientApplicationFactory,
+    ) {
+        this.tokenScope = `api://${resourceClientId}/.default`;
+        const config: Configuration = {
+            auth: {
+                clientId: clientId,
+                clientSecret: clientSecret,
+                authority: authorityUrl,
+            },
+        };
+        this.app = clientApplicationFactory(config);
+    }
+
+    public async getAuthHeaders(): Promise<Headers> {
+        const accessToken = await this.getToken();
+
+        return {
+            authorization: `${accessToken.tokenType} ${accessToken.accessToken}`,
+        };
+    }
+
+    private async getToken(): Promise<AuthenticationResult> {
+        return this.retryHelper.executeWithRetries(
+            () =>
+                this.app.acquireTokenByClientCredential({
+                    scopes: [this.tokenScope],
+                }),
+            async (error) => this.logger.logError(`Error while acquiring Azure AD client token. ${System.serializeError(error)}`),
+            this.maxTokenAttempts,
+            this.msecBetweenRetries,
+        );
+    }
+}
