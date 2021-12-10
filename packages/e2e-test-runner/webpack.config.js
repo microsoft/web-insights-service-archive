f6ab7b0edf8e9fce3be98e8ac87568b34fc6f2dd
Kelsey Pownell <kepownel@microsoft.com>
2021-12-10 12:27:48 -0800

    Create new package for e2e test runner

---
+++ packages/e2e-test-runner/webpack.config.js
@@ -0,0 +1,82 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const path = require('path');
+const webpack = require('webpack');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const copyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = (env) => {
+    let version = 'dev';
+    if (env && env.version) {
+        version = env.version;
+    }
+    console.log(`Building for version : ${version}`);
+
+    return {
+        devtool: 'cheap-source-map',
+        entry: {
+            ['e2e-test-runner']: path.resolve('./src/index.ts'),
+        },
+        mode: 'development',
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: [
+                        {
+                            loader: 'ts-loader',
+                            options: {
+                                transpileOnly: true,
+                                experimentalWatchApi: true,
+                            },
+                        },
+                    ],
+                    exclude: ['/node_modules/', /\.(spec|e2e)\.ts$/],
+                },
+                {
+                    test: /\.node$/,
+                    use: [
+                        {
+                            loader: 'node-loader',
+                        },
+                    ],
+                },
+            ],
+        },
+        name: 'e2e-test-runner',
+        node: {
+            __dirname: false,
+        },
+        output: {
+            path: path.resolve('./dist'),
+            filename: '[name].js',
+            libraryTarget: 'commonjs2',
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                __IMAGE_VERSION__: JSON.stringify(version),
+            }),
+            new ForkTsCheckerWebpackPlugin(),
+            new copyWebpackPlugin({
+                patterns: [
+                    {
+                        context: './docker-image-config',
+                        from: '.dockerignore',
+                        to: '',
+                    },
+                    {
+                        context: './docker-image-config',
+                        from: 'Dockerfile',
+                        to: '',
+                    },
+                ],
+            }),
+        ],
+        resolve: {
+            extensions: ['.ts', '.js', '.json'],
+            mainFields: ['main'],
+        },
+        target: 'node',
+    };
+};
