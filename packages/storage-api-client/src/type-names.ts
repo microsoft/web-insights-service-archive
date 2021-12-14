da870001beb0b913a38e8bdcc8144dc364deb39f
Kelsey Pownell <kepownel@microsoft.com>
2021-12-14 14:54:26 -0800

    code cleanup, use a different Linux container image

--- packages/storage-api-client/src/type-names.ts
+++ packages/storage-api-client/src/type-names.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { WebInsightsStorageClient } from '.';
+import { WebInsightsStorageClient } from './web-insights-storage-client';
 
 export const WebInsightsServiceClientTypeNames = {
     WebInsightsClientProvider: 'WebInsightsClientProvider',
